58c294647c838113885a912b9991529182693904
Eric Rouse <rousee@onid.oregonstate.edu>
2015-02-01 22:07:42 -0800

    favorites are at ~99%

--- part2/script.js
+++ part2/script.js
@@ -8,6 +8,7 @@ function paginate(pages_requested, filters) {
         if (httpRequest.readyState === 4) {
           if (httpRequest.status === 200) {
             var text = JSON.parse(httpRequest.responseText);
+            var faves = listFavorites();
             var results = [];
             var space = "";
             for (var i = 0; i < text.length; i++){
@@ -23,12 +24,13 @@ function paginate(pages_requested, filters) {
                     var files = Object.keys(text[i].files);
                     for (var j = 0; j < files.length; j++){
                         console.log(text[i].files[files[j]].language);
-                        if (filters.indexOf(text[i].files[files[j]].language) != -1){
+                        if (filters.indexOf(text[i].files[files[j]].language) != -1 && faves.indexOf(text[i].html_url) === -1){
                             results.push("<a href=\"" + text[i].html_url + "\"> gist" + space + (overall+i) + "-->" + text[i].description + "</a><br>");
+                            break;
                         }
                     }
                     
-                } else {
+                } else if (faves.indexOf(text[i].html_url) === -1) {
                     results.push("<a href=\"" + text[i].html_url + "\"> gist" + space + (overall+i) + "-->" + text[i].description + "</a><br>");
                 }
 
@@ -60,11 +62,12 @@ function paginate(pages_requested, filters) {
 
 window.onload = function() {
     document.getElementById('id1').innerHTML = "Optional, enter desired number of pages of results.<br>Optional, pick filters.<br>Or, just click search!";
+    document.getElementById('favorites').innerHTML = "Pick up to 5 favorites, paste their URLs below."
     //getGistList();
 }
 
 function displayLocalStorage(id) {
-    var storedList = JSON.parse(localStorage.getItem('storage1'));
+    var storedList = JSON.parse(localStorage.getItem('json_store'));
     var htmlstring = "";
     for (var i = 0; i < storedList.length; i++){
         htmlstring += storedList[i];
@@ -72,8 +75,27 @@ function displayLocalStorage(id) {
     document.getElementById(id).innerHTML = htmlstring;
 }
 
+function displayFavorites() {
+    var storedList = JSON.parse(localStorage.getItem('fav_store'));
+    var htmlstring = "";
+    for (var i = 0; i < storedList.length; i++){
+        htmlstring += "favorite " + (i+1) + ". "
+        htmlstring += storedList[i];
+        htmlstring += "<br>";
+    }
+    document.getElementById('favorites').innerHTML = htmlstring;
+}
+
+function listFavorites() {
+    return JSON.parse(localStorage.getItem('fav_store'));
+}
+
 function saveLocalStorage(items) {
-    localStorage.setItem('storage1', JSON.stringify(items));
+    localStorage.setItem('json_store', JSON.stringify(items));
+}
+
+function saveFavorites(faves) {
+    localStorage.setItem('fav_store', JSON.stringify(faves));
 }
 
 function clearLocalStorage() {
@@ -84,17 +106,28 @@ function searchTheThings(){
     //grab the number of pages they want
     var sc = parseInt(document.getElementsByName('searchcount')[0].value, 10);
     var cb = document.getElementsByName('filter');
-    var filters = []
+    var filters = [];
     for (var i = 0; i < cb.length; i++){
         if (cb[i].checked){
-            filters.push(cb[i].id)
+            filters.push(cb[i].id);
         }
     }
     //throw away anything that isn't a number from 1 to 5
     if (isNaN(sc) || (sc > 5) || (sc < 1)) {
         sc = 1;
     }
+    saveTheFaves();
     paginate(sc, filters);
     console.log(cb, filters);//debug
 
+}
+
+function saveTheFaves(){
+    var tb = document.getElementsByName('favorite');
+    var favorites = [];
+    for (var i = 0; i < tb.length; i++){
+        favorites.push(tb[i].value);
+    }
+    saveFavorites(favorites);
+    displayFavorites();
 }
